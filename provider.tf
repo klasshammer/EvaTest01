58f47f269e86e085cddbad61a608310f65050d17
klasshammer <cmaureiravargas@icloud.com>
2026-04-02 23:24:08 -0300

    Add Terraform configuration for AWS provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.39.0"
+    }
+  }
+}
+
+
+
 provider "aws" {
   region = "us-east-1"
 }
